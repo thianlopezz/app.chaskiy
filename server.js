c18f47e4eccabb81817ffe80e9b69af4773b5d7e
thianlopezz <thianlopezz@gmail.com>
2017-07-16 02:04:49 -0500

    deploy8

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 app.set('port', port);
 
 //mongoose.connect('mongodb://'+config.mongodb.user+':'+config.mongodb.password+'@'+config.mongodb.host+':'+config.mongodb.port+'/'+config.mongodb.db);
