657525e92ce25e14ff3c9778bdfe9fccee5c552b
thianlopezz <thianlopezz@gmail.com>
2018-08-22 01:02:10 -0500

    para primera subida ec2

--- api/connection.js
+++ api/connection.js
@@ -2,45 +2,20 @@ var mysql = require('mysql');
 const process = require('process');
 
 function Connection() {
-  this.pool = null;
 
+  this.pool = null;
 
   this.init = function () {
 
-
-    let config = {};
-
-    if (process.env.SQL_USER) {
-
-      config = {
-        connectionLimit: 10,
-        user: process.env.SQL_USER,
-        password: process.env.SQL_PASSWORD,
-        database: process.env.SQL_DATABASE
-      };
-    } else {
-      // config = {
-      //   connectionLimit: 10,
-      //   host: '35.199.127.98',
-      //   port: 3306,
-      //   user: 'root',
-      //   password: 'admin',
-      //   database: 'chaskiy-db'
-      // };
-      config = {
-        connectionLimit: 10,
+    let config = {
+        connectionLimit: 15,
         host: 'localhost',
         user: 'root',
         password: 'password',
-        database: 'chaskiy-db'
+        database: 'chaskiy'
       };
-    }
 
-    if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-      config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-    }
-
-    console.log('Conne>>params>>');
+    console.log('Conne>> params>>');
     console.log(config);
 
     this.pool = mysql.createPool(
