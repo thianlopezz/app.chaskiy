947f64de594a3cbb4cf63bf6f7324db560bee991
thianlopezz <thianlopezz@gmail.com>
2019-02-06 07:36:34 -0500

    variables de entorno, se configuro desde fuente hasta solucionar en servidor0

--- api/connection.js
+++ api/connection.js
@@ -8,6 +8,7 @@ function Connection() {
     let config = {
       connectionLimit: 15,
       host: process.env.MYSQL_HOST || "localhost",
+      port: process.env.MYSQL_PORT || 3306,
       user: process.env.MYSQL_USER || "root",
       password: process.env.MYSQL_PASSWORD || "cha$key16",
       database: process.env.MYSQL_CHASKIY || "chaskiy"
