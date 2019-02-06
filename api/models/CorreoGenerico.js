77713d69813b8610d95e512bf8381e0378b5d660
thianlopezz <thianlopezz@gmail.com>
2019-02-05 22:55:04 -0500

    configuracion de variables de entorno

--- api/models/CorreoGenerico.js
+++ api/models/CorreoGenerico.js
@@ -1,35 +1,33 @@
-
-const axios = require('axios');
+const axios = require("axios");
 
 // const API = process.env.CORREO_GENERICO || 'https://correo-generico.herokuapp.com';
-const API = process.env.CORREO_GENERICO || 'http://localhost:3000';
+const API = process.env.CORREO_GENERICO || "http://localhost:3000";
 
-const TOKEN_CORREO = process.env.TOKEN_CORREO || '123';
+const TOKEN_CORREO = process.env.TOKEN_CORREO || "123";
 
 function CorreoGenerico() {
+  this.enviar = function(asunto, destinatario, claves, plantilla, idHospedaje) {
+    var correo = {
+      idHospedaje,
+      asunto: asunto,
+      destinatario: destinatario,
+      claves: claves,
+      plantilla: plantilla,
+      token: TOKEN_CORREO
+    };
 
-    this.enviar = function (asunto, destinatario, claves, plantilla, idHospedaje) {
-        
-        var correo = {
-            idHospedaje,
-            asunto: asunto,
-            destinatario: destinatario,
-            claves: claves,
-            plantilla: plantilla,
-            token: TOKEN_CORREO
-        };
-
-        return new Promise(function(resolve, reject){
-            axios.post(`${API}/api/send/v2`, correo)
-                .then(() => {
-                    resolve(correo);
-                })
-                .catch(error => {
-                    console.log("Err>>" + error);
-                    reject(error);
-                });
+    return new Promise(function(resolve, reject) {
+      axios
+        .post(`${API}/api/send/v2`, correo)
+        .then(() => {
+          resolve(correo);
         })
-    }
+        .catch(error => {
+          console.log("Err>>" + error);
+          reject(error);
+        });
+    });
+  };
 }
 
 module.exports = new CorreoGenerico();
